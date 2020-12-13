caf2bec2764935d24e58e365df9d9841bd88166f
Quinn Conrad <quinnconrad15@gmail.com>
2020-12-13 15:25:16 -0600

    Basic UI layout

--- app/src/main/java/com/severeweatheralerts/AlertViewerActivity.java
+++ app/src/main/java/com/severeweatheralerts/AlertViewerActivity.java
@@ -2,7 +2,10 @@ package com.severeweatheralerts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AlertViewerActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AlertViewerActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_alert_viewer);
+
+    makeStatusBarTransparent();
+  }
+
+  private void makeStatusBarTransparent() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      Window w = getWindow();
+      w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
   }
 }
