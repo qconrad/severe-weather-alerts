8c8d148117a349ea51e083e90e0d15b81fc46d2b
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-10 17:22:18 -0600

    Removed side effects from reference filter

--- app/src/main/java/com/severeweatheralerts/ReferenceFilter.java
+++ app/src/main/java/com/severeweatheralerts/ReferenceFilter.java
@@ -7,9 +7,14 @@ import java.util.ArrayList;
 // Want to keep minimum android version old so not using java's filtering
 public class ReferenceFilter {
   public static ArrayList<Alert> removeReferences(ArrayList<Alert> alerts) {
-    for (int i = 0; i < alerts.size(); i++)
-      if (shouldFilterAlert(alerts.get(i))) alerts.remove(i--);
-    return alerts;
+    ArrayList<Alert> output = getCopy(alerts);
+    for (int i = 0; i < output.size(); i++)
+      if (shouldFilterAlert(output.get(i))) output.remove(i--);
+    return output;
+  }
+
+  private static ArrayList<Alert> getCopy(ArrayList<Alert> alerts) {
+    return (ArrayList<Alert>) alerts.clone();
   }
 
   protected static boolean shouldFilterAlert(Alert alert) {
--- app/src/test/java/com/severeweatheralerts/ReferenceFilterTests.java
+++ app/src/test/java/com/severeweatheralerts/ReferenceFilterTests.java
@@ -43,4 +43,19 @@ public class ReferenceFilterTests {
     alerts.add(alert3);
     assertEquals(1, ReferenceFilter.removeReferences(alerts).size());
   }
+
+  @Test
+  public void removeReferences_SecondAndThirdReplaced_OriginalListNotAffected() {
+    ArrayList<Alert> alerts = new ArrayList<>();
+    DefaultAlert alert1 = new DefaultAlert();
+    DefaultAlert alert2 = new DefaultAlert();
+    DefaultAlert alert3 = new DefaultAlert();
+    alert2.setReplaced(true);
+    alert3.setReplaced(true);
+    alerts.add(alert1);
+    alerts.add(alert2);
+    alerts.add(alert3);
+    ReferenceFilter.removeReferences(alerts);
+    assertEquals(3, alerts.size());
+  }
 }
