d36a94bad98644df3e8075d6578e652659a87b39
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-16 16:47:11 -0600

    User sync string generator

---
+++ app/src/main/java/com/severeweatheralerts/UserSyncJSONGenerator.java
@@ -0,0 +1,13 @@
+package com.severeweatheralerts;
+
+public class UserSyncJSONGenerator {
+  private final String token;
+
+  public UserSyncJSONGenerator(String token) {
+    this.token = token;
+  }
+
+  public String getLocationsString(String locationJSON) {
+    return "{\"" + token + "\":{\"locations\":" + locationJSON + "}}";
+  }
+}
---
+++ app/src/test/java/com/severeweatheralerts/UserSyncJSONGeneratorTests.java
@@ -0,0 +1,19 @@
+package com.severeweatheralerts;
+
+import com.severeweatheralerts.Location.Location;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserSyncJSONGeneratorTests {
+  @Test
+  public void getString_TokenAndLocationsProvided_ValidJSONReturned() {
+    String token = "0123456789abc";
+    UserSyncJSONGenerator userSyncJSONGenerator = new UserSyncJSONGenerator(token);
+    String output = userSyncJSONGenerator.getLocationsString(new JSONLocationString(new ArrayList<Location>()).getString());
+    assertEquals("{\"0123456789abc\":{\"locations\":[[]]}}", output);
+  }
+}
