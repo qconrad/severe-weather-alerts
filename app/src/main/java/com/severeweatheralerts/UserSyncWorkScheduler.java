5aad8d6203079d74ca93581085b5589fc4cfec6b
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-18 00:02:24 -0600

    Added scheduler class

---
+++ app/src/main/java/com/severeweatheralerts/UserSyncWorkScheduler.java
@@ -0,0 +1,35 @@
+package com.severeweatheralerts;
+
+import android.content.Context;
+
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
+public class UserSyncWorkScheduler {
+  private final Context context;
+
+  public UserSyncWorkScheduler(Context context) {
+    this.context = context;
+  }
+
+  public void oneTimeSync() {
+    Constraints constraints = getConstraints();
+    WorkRequest userSync = getWorkRequest(constraints);
+    WorkManager.getInstance(context).enqueue(userSync);
+  }
+
+  private WorkRequest getWorkRequest(Constraints constraints) {
+    return new OneTimeWorkRequest.Builder(UserSyncWorker.class)
+            .setConstraints(constraints)
+            .build();
+  }
+
+  private Constraints getConstraints() {
+    return new Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build();
+  }
+}
