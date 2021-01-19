f05b5afb7df0f6b3880f0740d74cc2898eab41f2
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-18 23:19:15 -0600

    Moved fetching to loading activity

--- app/src/main/java/com/severeweatheralerts/Activities/AlertListActivity.java
+++ app/src/main/java/com/severeweatheralerts/Activities/AlertListActivity.java
@@ -2,7 +2,6 @@ package com.severeweatheralerts.Activities;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Pair;
@@ -13,13 +12,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.severeweatheralerts.Location.LocationsDao;
-import com.severeweatheralerts.Networking.LocationPopulaters.FromLocationPointPopulater;
 import com.severeweatheralerts.R;
 import com.severeweatheralerts.RecyclerViews.Alert.AlertCardHolder;
 import com.severeweatheralerts.RecyclerViews.Alert.AlertRecyclerViewAdapter;
 import com.severeweatheralerts.UserSync.UserSyncWorkScheduler;
-
-import java.io.IOException;
 
 import static com.severeweatheralerts.AlertListTools.AlertFilter.filter;
 
@@ -28,16 +24,12 @@ public class AlertListActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_alertlist);
-    asyncRefresh();
+    populateRecyclerView();
     userSync();
   }
 
   private void userSync() {
     new UserSyncWorkScheduler(this).oneTimeSync();
-  }
-
-  private void asyncRefresh() {
-    new AsyncRefresh().execute();
   }
 
   private void displayFullAlert(int alertIndex, RecyclerView.ViewHolder holder) {
@@ -60,23 +52,5 @@ public class AlertListActivity extends AppCompatActivity {
     AlertRecyclerViewAdapter alertRecyclerViewAdapter = new AlertRecyclerViewAdapter(filter(LocationsDao.getLocation(0).getAlerts()));
     alertRecyclerViewAdapter.setClickListener(this::displayFullAlert);
     recyclerView.setAdapter(alertRecyclerViewAdapter);
-  }
-
-  private class AsyncRefresh extends AsyncTask<Void, Void, Void> {
-    @Override
-    protected Void doInBackground(Void... params) {
-      try { populateAlerts(); }
-      catch (IOException e) { e.printStackTrace(); }
-      return null;
-    }
-
-    private void populateAlerts() throws IOException {
-      new FromLocationPointPopulater(LocationsDao.getLocation(0)).populate();
-    }
-
-    @Override
-    protected void onPostExecute(Void result) {
-      populateRecyclerView();
-    }
   }
 }
--- app/src/main/java/com/severeweatheralerts/Activities/LoadingActivity.java
+++ app/src/main/java/com/severeweatheralerts/Activities/LoadingActivity.java
@@ -3,14 +3,18 @@ package com.severeweatheralerts.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.severeweatheralerts.Location.GPSLocation;
 import com.severeweatheralerts.Location.LastKnownLocation;
 import com.severeweatheralerts.Location.Location;
 import com.severeweatheralerts.Location.LocationsDao;
+import com.severeweatheralerts.Networking.LocationPopulaters.FromLocationPointPopulater;
 import com.severeweatheralerts.PermissionManager;
 import com.severeweatheralerts.R;
+
+import java.io.IOException;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -32,11 +36,17 @@ public class LoadingActivity extends AppCompatActivity {
 
   private void useGPS(Location deviceLoc) {
     gps = new GPSLocation(this, location -> {
-      if (location.getAccuracy() < 100) {
-        gps.stopUpdates();
-        setDeviceLocation(deviceLoc, location);
-      }
+      if (accurateEnough(location)) useLocation(deviceLoc, location);
     }).startUpdates();
+  }
+
+  private void useLocation(Location deviceLoc, android.location.Location location) {
+    gps.stopUpdates();
+    setDeviceLocation(deviceLoc, location);
+  }
+
+  private boolean accurateEnough(android.location.Location location) {
+    return location.getAccuracy() < 100;
   }
 
   private void checkPermissions() {
@@ -46,16 +56,39 @@ public class LoadingActivity extends AppCompatActivity {
   private void setDeviceLocation(Location deviceLoc, android.location.Location location) {
     adaptLocation(deviceLoc, location);
     LocationsDao.addLocation(deviceLoc);
-    displayAlerts();
+    getAlerts();
+  }
+
+  private void getAlerts() {
+    new AsyncRefresh().execute();
   }
 
   private void displayAlerts() {
     Intent alertListIntent = new Intent(LoadingActivity.this, AlertListActivity.class);
     startActivity(alertListIntent);
+    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
   }
 
   private void adaptLocation(Location loc, android.location.Location deviceLoc) {
     loc.setLatitude(deviceLoc.getLatitude());
     loc.setLongitude(deviceLoc.getLongitude());
+  }
+
+  private class AsyncRefresh extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... params) {
+      try { populateAlerts(); }
+      catch (IOException e) { e.printStackTrace(); }
+      return null;
+    }
+
+    private void populateAlerts() throws IOException {
+      new FromLocationPointPopulater(LocationsDao.getLocation(0)).populate();
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+      displayAlerts();
+    }
   }
 }
