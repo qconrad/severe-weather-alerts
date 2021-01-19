357d63519432ee4cd50941d16c5f7ac135807654
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-18 22:35:51 -0600

    Added loading activity

---
+++ app/src/main/java/com/severeweatheralerts/Activities/LoadingActivity.java
@@ -0,0 +1,16 @@
+package com.severeweatheralerts.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.severeweatheralerts.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_loading);
+  }
+}
