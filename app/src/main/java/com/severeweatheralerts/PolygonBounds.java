674cca9e75ce930ef8810aaa953210f00c6dcf14
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-20 04:53:13 -0600

    Bound object

---
+++ app/src/main/java/com/severeweatheralerts/PolygonBounds.java
@@ -0,0 +1,20 @@
+package com.severeweatheralerts;
+
+public class PolygonBounds {
+  private final double top;
+  private final double right;
+  private final double bottom;
+  private final double left;
+
+  public PolygonBounds(double top, double right, double bottom, double left) {
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.left = left;
+  }
+
+  public double getTop() { return top; }
+  public double getRight() { return right; }
+  public double getBottom() { return bottom; }
+  public double getLeft() { return left; }
+}
