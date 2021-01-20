f0dc8b369191bc267565faf44f2d6e369d8ad0af
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-19 20:26:43 -0600

    Polygon class

---
+++ app/src/main/java/com/severeweatheralerts/Polygon.java
@@ -0,0 +1,19 @@
+package com.severeweatheralerts;
+
+import java.util.ArrayList;
+
+public class Polygon {
+  private final ArrayList<MercatorCoordinate> coordList = new ArrayList<>();
+
+  public void addCoordinate(MercatorCoordinate mercatorCoordinate) {
+    coordList.add(mercatorCoordinate);
+  }
+
+  public int getCoordinateCount() {
+    return coordList.size();
+  }
+
+  public MercatorCoordinate getCoordinate(int index) {
+    return coordList.get(index);
+  }
+}
---
+++ app/src/test/java/com/severeweatheralerts/PolygonTests.java
@@ -0,0 +1,70 @@
+package com.severeweatheralerts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PolygonTests {
+  @Test
+  public void getCoordinateCount_NothingAdded_CountIs0() {
+    assertEquals(0, new Polygon().getCoordinateCount());
+  }
+
+  @Test
+  public void getCoordinateCount_OneCoordinateAdded_CountIs1() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(0.0, 0.0));
+    assertEquals(1, polygon.getCoordinateCount());
+  }
+
+  @Test
+  public void getCoordinateCount_TwoCoordinateAdded_CountIs2() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(0.0, 0.0));
+    polygon.addCoordinate(new MercatorCoordinate(0.0, 0.0));
+    assertEquals(2, polygon.getCoordinateCount());
+  }
+
+  @Test
+  public void getCoordinate_OneCoordinateIsAdded_CorrectCoordinateReturn() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(1.0, 2.0));
+    assertEquals(1, polygon.getCoordinate(0).getX(), 0.001);
+    assertEquals(2, polygon.getCoordinate(0).getY(), 0.001);
+  }
+
+  @Test
+  public void getCoordinate_DifferentCoordinateIsAdded_CorrectCoordinateReturn() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(2.0, 1.0));
+    assertEquals(2, polygon.getCoordinate(0).getX(), 0.001);
+    assertEquals(1, polygon.getCoordinate(0).getY(), 0.001);
+  }
+
+  @Test
+  public void getCoordinate_TwoCoordinatesIsAdded_FirstCorrectCoordinateReturn() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(2.0, 1.0));
+    polygon.addCoordinate(new MercatorCoordinate(1.0, 2.0));
+    assertEquals(2, polygon.getCoordinate(0).getX(), 0.001);
+    assertEquals(1, polygon.getCoordinate(0).getY(), 0.001);
+  }
+
+  @Test
+  public void getCoordinate_DifferentCoordinateIsAdded_FirstCorrectCoordinateReturn() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(2.0, 1.0));
+    polygon.addCoordinate(new MercatorCoordinate(1.0, 2.0));
+    assertEquals(2, polygon.getCoordinate(0).getX(), 0.001);
+    assertEquals(1, polygon.getCoordinate(0).getY(), 0.001);
+  }
+
+  @Test
+  public void getCoordinate_DifferentCoordinateIsAdded_SecondCorrectCoordinateReturn() {
+    Polygon polygon = new Polygon();
+    polygon.addCoordinate(new MercatorCoordinate(2.0, 1.0));
+    polygon.addCoordinate(new MercatorCoordinate(1.0, 2.0));
+    assertEquals(1, polygon.getCoordinate(1).getX(), 0.001);
+    assertEquals(2, polygon.getCoordinate(1).getY(), 0.001);
+  }
+}
