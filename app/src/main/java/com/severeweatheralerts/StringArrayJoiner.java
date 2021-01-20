3cb25827c6910416d99605c763c06e3a0ada6b53
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-19 18:28:07 -0600

    String joiner class

---
+++ app/src/main/java/com/severeweatheralerts/StringArrayJoiner.java
@@ -0,0 +1,24 @@
+package com.severeweatheralerts;
+
+public class StringArrayJoiner {
+  private final String[] arr;
+
+  public StringArrayJoiner(String[] arr) {
+    this.arr = arr;
+  }
+
+  public String join() {
+    StringBuilder joinedString = new StringBuilder();
+    for (int i = 0; i < arr.length; i++) joinItem(joinedString, i);
+    return joinedString.toString();
+  }
+
+  private void joinItem(StringBuilder joinedString, int i) {
+    joinedString.append(arr[i]);
+    if (notLastItem(i)) joinedString.append(" ");
+  }
+
+  private boolean notLastItem(int i) {
+    return i < arr.length - 1;
+  }
+}
---
+++ app/src/test/java/com/severeweatheralerts/StringArrayJoinerTests.java
@@ -0,0 +1,43 @@
+package com.severeweatheralerts;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringArrayJoinerTests {
+  @Test
+  public void join_emptyArray_EmptyStringReturn() {
+    String[] testArr = {};
+    assertEquals("", new StringArrayJoiner(testArr).join());
+  }
+
+  @Test
+  public void join_OneItemInArray_ThatItemReturn() {
+    String[] testArr = {"test"};
+    assertEquals("test", new StringArrayJoiner(testArr).join());
+  }
+
+  @Test
+  public void join_TwoItemsInArray_ThoseItemsReturnInOneString() {
+    String[] testArr = {"test", "test2"};
+    assertEquals("test test2", new StringArrayJoiner(testArr).join());
+  }
+
+  @Test
+  public void join_DifferentItem_CorrectStringReturn() {
+    String[] testArr = {"newString"};
+    assertEquals("newString", new StringArrayJoiner(testArr).join());
+  }
+
+  @Test
+  public void join_DifferentTwoItems_CorrectItemsReturned() {
+    String[] testArr = {"newString", "newString2"};
+    assertEquals("newString newString2", new StringArrayJoiner(testArr).join());
+  }
+
+  @Test
+  public void join_ThreeItemsProvided_AllInString() {
+    String[] testArr = {"newString", "newString2", "newString3"};
+    assertEquals("newString newString2 newString3", new StringArrayJoiner(testArr).join());
+  }
+}
