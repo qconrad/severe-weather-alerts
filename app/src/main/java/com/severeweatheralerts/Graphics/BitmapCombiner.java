92c324742e3b399cc8d011ffd9783a4e61c7c6ea
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-27 02:27:06 -0600

    Added bitmap combiner class

---
+++ app/src/main/java/com/severeweatheralerts/Graphics/BitmapCombiner.java
@@ -0,0 +1,23 @@
+package com.severeweatheralerts.Graphics;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+
+import java.util.ArrayList;
+
+public class BitmapCombiner {
+  private final ArrayList<Bitmap> bitmaps;
+
+  public BitmapCombiner(ArrayList<Bitmap> bitmaps) {
+    this.bitmaps = bitmaps;
+  }
+
+  public Bitmap combine() {
+    Bitmap combined = Bitmap.createBitmap(bitmaps.get(0).getWidth(), bitmaps.get(0).getHeight(), bitmaps.get(0).getConfig());
+    Canvas canvas = new Canvas(combined);
+    for (int i = 0; i < bitmaps.size(); i++)
+      canvas.drawBitmap(bitmaps.get(i), new Matrix(), null);
+    return combined;
+  }
+}
