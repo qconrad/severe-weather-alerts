5714a52986e38843c3ef17a9868679cf0b4b6a4d
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-26 20:06:31 -0600

    Added multi polygon to geometry parser

--- app/src/androidTest/java/com/severeweatheralerts/GeometryParserTests.java
+++ app/src/androidTest/java/com/severeweatheralerts/GeometryParserTests.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 public class GeometryParserTests {
   String singlePolygonGeometry = "{\"type\":\"Polygon\",\"coordinates\":[[[-73.59,44.26],[-73.31,44.46],[-72.9,44.37],[-73.36,44],[-73.59,44.26]]]}";
+  String multiPolygonGeometry = "{ \"type\":\"MultiPolygon\", \"coordinates\": [[[[ -84.6136932, 34.081111900000003 ], [ -84.613494799999998, 34.077213200000003 ], [ -84.588394100000002, 34.076610500000001 ], [ -84.566596900000008, 34.076312999999999 ]]]] }";
   @Test
   public void parseAlerts_GeometryGiven_FirstCoordLatitudeParsed() throws JSONException {
     GeometryParser geometryParser = new GeometryParser(new JSONObject(singlePolygonGeometry));
@@ -38,5 +39,17 @@ public class GeometryParserTests {
   public void parseAlerts_GeometryGiven_ThirdCoordLatitudeCorrect() throws JSONException {
     GeometryParser geometryParser = new GeometryParser(new JSONObject(singlePolygonGeometry));
     assertEquals(44.37, geometryParser.parseGeometry().get(0).getCoordinate(2).getLat(), 0.01);
+  }
+
+  @Test
+  public void parseAlerts_MultiPolygonGeometryGiven_FirstCoordinateLatIsCorrect() throws JSONException {
+    GeometryParser geometryParser = new GeometryParser(new JSONObject(multiPolygonGeometry));
+    assertEquals(34.081, geometryParser.parseGeometry().get(0).getCoordinate(0).getLat(), 0.01);
+  }
+
+  @Test
+  public void parseAlerts_MultiPolygonGeometryGiven_FirstCoordinateLongIsCorrect() throws JSONException {
+    GeometryParser geometryParser = new GeometryParser(new JSONObject(multiPolygonGeometry));
+    assertEquals(-84.613, geometryParser.parseGeometry().get(0).getCoordinate(0).getLong(), 0.01);
   }
 }
--- app/src/main/java/com/severeweatheralerts/JSONParsing/GeometryParser.java
+++ app/src/main/java/com/severeweatheralerts/JSONParsing/GeometryParser.java
@@ -3,6 +3,7 @@ package com.severeweatheralerts.JSONParsing;
 import com.severeweatheralerts.Adapters.GCSCoordinate;
 import com.severeweatheralerts.Adapters.GeoJSONPolygon;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,17 +19,33 @@ public class GeometryParser {
 
   public ArrayList<GeoJSONPolygon> parseGeometry() throws JSONException {
     GeoJSONPolygon geoJSONPolygon = new GeoJSONPolygon();
-    for (int i = 0; i < geometry.getJSONArray("coordinates").getJSONArray(0).length(); i++)
-      geoJSONPolygon.addCoordinate(new GCSCoordinate(getLat(i), getLon(i)));
+    for (int i = 0; i < getPolygon().length(); i++)
+      geoJSONPolygon.addCoordinate(new GCSCoordinate(
+              getLat(getPolygon().getJSONArray(i)),
+              getLon(getPolygon().getJSONArray(i))));
     polygons.add(geoJSONPolygon);
     return polygons;
   }
 
-  private double getLon(int i) throws JSONException {
-    return geometry.getJSONArray("coordinates").getJSONArray(0).getJSONArray(i).getDouble(0);
+  private JSONArray getPolygon() throws JSONException {
+    JSONArray polygon = getCoordinates().getJSONArray(0);
+    if (isMultiPolygon()) polygon = polygon.getJSONArray(0);
+    return polygon;
   }
 
-  private double getLat(int i) throws JSONException {
-    return geometry.getJSONArray("coordinates").getJSONArray(0).getJSONArray(i).getDouble(1);
+  private boolean isMultiPolygon() throws JSONException {
+    return geometry.get("type").equals("MultiPolygon");
+  }
+
+  private JSONArray getCoordinates() throws JSONException {
+    return geometry.getJSONArray("coordinates");
+  }
+
+  private double getLon(JSONArray coordinate) throws JSONException {
+    return coordinate.getDouble(0);
+  }
+
+  private double getLat(JSONArray coordinate) throws JSONException {
+    return coordinate.getDouble(1);
   }
 }
