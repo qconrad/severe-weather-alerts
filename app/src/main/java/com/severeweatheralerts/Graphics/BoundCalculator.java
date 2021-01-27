74c2dd0faed8563506802c3a436aedfaba0f9ff9
Quinn Conrad <quinnconrad15@gmail.com>
2021-01-26 21:13:36 -0600

    Refactored bound calculator to use min and max functions

--- app/src/main/java/com/severeweatheralerts/Graphics/BoundCalculator.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/BoundCalculator.java
@@ -19,14 +19,10 @@ public class BoundCalculator {
     double bottom = Double.POSITIVE_INFINITY;
     double left = Double.POSITIVE_INFINITY;
     for (int i = 0; i < polygon.getCoordinateCount(); i++) {
-      if (polygon.getCoordinate(i).getX() < left)
-        left = polygon.getCoordinate(i).getX();
-      if (polygon.getCoordinate(i).getY() < bottom)
-        bottom = polygon.getCoordinate(i).getY();
-      if (polygon.getCoordinate(i).getY() > top)
-        top = polygon.getCoordinate(i).getY();
-      if (polygon.getCoordinate(i).getX() > right)
-        right = polygon.getCoordinate(i).getX();
+      left = Math.min(left, polygon.getCoordinate(i).getX());
+      bottom = Math.min(bottom, polygon.getCoordinate(i).getY());
+      top = Math.max(top, polygon.getCoordinate(i).getY());
+      right = Math.max(right, polygon.getCoordinate(i).getX());
     }
     bounds = new Bounds(top, right, bottom, left);
   }
