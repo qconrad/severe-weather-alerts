571a8ddeb8a26ab5383a8671fb3411a8c1cf29e7
Quinn Conrad <quinnconrad15@gmail.com>
2021-02-06 20:11:02 -0600

    Bound Margin class

---
+++ app/src/main/java/com/severeweatheralerts/BoundMargin.java
@@ -0,0 +1,41 @@
+package com.severeweatheralerts;
+
+import com.severeweatheralerts.Graphics.Bounds;
+
+public class BoundMargin {
+  private final Bounds bounds;
+  private final double marginPercentage;
+
+  public BoundMargin(Bounds bounds, double marginPercentage) {
+    this.bounds = bounds;
+    this.marginPercentage = marginPercentage;
+  }
+
+  public Bounds getBounds() {
+    return new Bounds(getTop(), getRight(), getBottom(), getLeft());
+  }
+
+  protected double getLeft() {
+    return bounds.getLeft() - getHorizontalMargin();
+  }
+
+  protected double getBottom() {
+    return bounds.getBottom() - getVerticalMargin();
+  }
+
+  protected double getRight() {
+    return bounds.getRight() + getHorizontalMargin();
+  }
+
+  protected double getTop() {
+    return bounds.getTop() + getVerticalMargin();
+  }
+
+  private double getHorizontalMargin() {
+    return (bounds.getRight() - bounds.getLeft()) * marginPercentage;
+  }
+
+  private double getVerticalMargin() {
+    return (bounds.getTop() - bounds.getBottom()) * marginPercentage;
+  }
+}
---
+++ app/src/test/java/com/severeweatheralerts/BoundMarginTests.java
@@ -0,0 +1,65 @@
+package com.severeweatheralerts;
+
+import com.severeweatheralerts.Graphics.Bounds;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoundMarginTests {
+  @Test
+  public void getBounds_MarginOfZero_SameBoundsReturned() {
+    Bounds bounds = new Bounds(0, 0, 0, 0);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0);
+    assertEquals(0.0, boundMargin.getBounds().getLeft(), 0.001);
+  }
+
+  @Test
+  public void getBounds_MarginOfZeroDifferentLeft_SameBoundsReturned() {
+    Bounds bounds = new Bounds(0, 0, 0, 1);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0);
+    assertEquals(1.0, boundMargin.getBounds().getLeft(), 0.001);
+  }
+
+  @Test
+  public void getBounds_50PercentMargin_LeftBoundCorrect() {
+    Bounds bounds = new Bounds(0, 0, 0, 10);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(15.0, boundMargin.getBounds().getLeft(), 0.001);
+  }
+
+  @Test
+  public void getBounds_50PercentMargin_RightBoundCorrect() {
+    Bounds bounds = new Bounds(0, 0, 0, -10);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(5.0, boundMargin.getBounds().getRight(), 0.001);
+  }
+
+  @Test
+  public void getBounds_OffCenter50PercentMargin_RightBoundCorrect() {
+    Bounds bounds = new Bounds(0, 15, 0, 5);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(20.0, boundMargin.getBounds().getRight(), 0.001);
+  }
+
+  @Test
+  public void getBounds_OffCenter50PercentMargin_LeftBoundCorrect() {
+    Bounds bounds = new Bounds(0, 15, 0, 5);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(0.0, boundMargin.getBounds().getLeft(), 0.001);
+  }
+
+  @Test
+  public void getBounds_50PercentMargin_TopBoundCorrect() {
+    Bounds bounds = new Bounds(10, 0, 0, 0);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(15.0, boundMargin.getBounds().getTop(), 0.001);
+  }
+
+  @Test
+  public void getBounds_50PercentMargin_BottomBoundCorrect() {
+    Bounds bounds = new Bounds(10, 0, 0, 0);
+    BoundMargin boundMargin = new BoundMargin(bounds, 0.5);
+    assertEquals(-5, boundMargin.getBounds().getBottom(), 0.001);
+  }
+}
