f46f337c26a4e93546cc97bb7adbb59e1b9221bb
Quinn Conrad <quinnconrad15@gmail.com>
2021-02-14 16:16:46 -0600

    Snowfall map urls

--- app/src/main/java/com/severeweatheralerts/Graphics/URLGenerator.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/URLGenerator.java
@@ -3,18 +3,11 @@ package com.severeweatheralerts.Graphics;
 import android.content.Context;
 
 import com.android.volley.VolleyError;
-import com.severeweatheralerts.Alerts.Alert;
 import com.severeweatheralerts.Constants;
 import com.severeweatheralerts.Networking.FetchServices.FetchCallback;
 import com.severeweatheralerts.Networking.FetchServices.StringFetchService;
-import com.severeweatheralerts.TextUtils.DateTimeConverter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.TimeZone;
 
 public class URLGenerator {
   private final GraphicType graphicType;
@@ -39,16 +32,15 @@ public class URLGenerator {
   }
 
   public void getMapTimes() {
-    ArrayList<Date> mapTimes = new ArrayList<>();
     StringFetchService fetchService = new StringFetchService(context, new URL().getMapTimes("snowamt", "conus"));
     fetchService.setUserAgent(Constants.USER_AGENT);
     fetchService.fetch(new FetchCallback() {
       @Override
       public void success(Object response) {
         MapTimeParser mapTimeParser = new MapTimeParser(response.toString());
-        ArrayList<Date> dates = mapTimeParser.getDates();
-        //urls.add(new URL().getTotalSnow(bounds, "conus", DateTimeConverter.convertDateToString(dates.get(0),"yyyy-MM-dd'T'HH:mm:ssX", TimeZone.getDefault())));
-        //urls.add(new URL().getTotalSnowPoints(bounds, "conus", times.getJSONArray(times.length()-1).getString(0)));
+        String LatestTime = mapTimeParser.getDateStrings().get(mapTimeParser.getDateStrings().size()-1);
+        urls.add(new URL().getTotalSnow(bounds, "conus", LatestTime));
+        urls.add(new URL().getTotalSnowPoints(bounds, "conus", LatestTime));
         urls.add(new URL().getCountyMap(bounds));
         completeListener.onComplete(urls);
       }
