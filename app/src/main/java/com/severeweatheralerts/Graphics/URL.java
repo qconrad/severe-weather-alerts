4ab5a7efda7ec2c6b47edc16ca2f6f711022ef21
Quinn Conrad <quinnconrad15@gmail.com>
2021-02-14 16:17:19 -0600

    County warning areas on county map

--- app/src/main/java/com/severeweatheralerts/Graphics/URL.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/URL.java
@@ -2,7 +2,7 @@ package com.severeweatheralerts.Graphics;
 
 public class URL {
   public String getCountyMap(Bounds bounds) {
-    return "https://digital.weather.gov/wms.php?LAYERS=counties,states&FORMAT=image%2Fpng&TRANSPARENT=TRUE&VERSION=1.3.0&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A3857&BBOX=" + bounds.getLeft() + "," + bounds.getBottom() + "," + bounds.getRight() + "," + bounds.getTop() + "&WIDTH=512&HEIGHT=512";
+    return "https://digital.weather.gov/wms.php?LAYERS=cwa,counties,states&FORMAT=image%2Fpng&VERSION=1.3.0&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A3857&BBOX=" + bounds.getLeft() + "," + bounds.getBottom() + "," + bounds.getRight() + "," + bounds.getTop() + "&WIDTH=512&HEIGHT=512";
   }
 
   public String getTotalSnow(Bounds bounds, String region, String date) {
