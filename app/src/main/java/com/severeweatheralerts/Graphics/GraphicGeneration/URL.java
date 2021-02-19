a4e213e402c9684d4078b77a278b9f6efa153fd5
Quinn Conrad <quinnconrad15@gmail.com>
2021-02-18 23:27:32 -0600

    Helper url method

--- app/src/main/java/com/severeweatheralerts/Graphics/GraphicGeneration/URL.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/GraphicGeneration/URL.java
@@ -8,11 +8,15 @@ public class URL {
   }
 
   public String getTotalSnow(Bound bounds, String region, String date) {
-    return "https://digital.weather.gov/wms.php?LAYERS=ndfd." + region + ".totalsnowamt&FORMAT=image%2Fpng&VERSION=1.3.0&VT=" + date + "&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A3857&BBOX=" + bounds.getLeft() + "," + bounds.getBottom() + "," + bounds.getRight() + "," + bounds.getTop() + "&WIDTH=512&HEIGHT=512";
+    return getParameter(bounds, region, date, "totalsnowamt");
   }
 
   public String getTotalSnowPoints(Bound bounds, String region, String date) {
-    return "https://digital.weather.gov/wms.php?LAYERS=ndfd." + region + ".totalsnowamt.points&FORMAT=image%2Fpng&VERSION=1.3.0&VT=" + date + "&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A3857&BBOX=" + bounds.getLeft() + "," + bounds.getBottom() + "," + bounds.getRight() + "," + bounds.getTop() + "&WIDTH=512&HEIGHT=512";
+    return getParameter(bounds, region, date, "totalsnowamt.points" );
+  }
+
+  private String getParameter(Bound bounds, String region, String date, String parameter) {
+    return "https://digital.weather.gov/wms.php?LAYERS=ndfd." + region + "." + parameter + "&FORMAT=image%2Fpng&VERSION=1.3.0&VT=" + date + "&EXCEPTIONS=INIMAGE&SERVICE=WMS&REQUEST=GetMap&STYLES=&CRS=EPSG%3A3857&BBOX=" + bounds.getLeft() + "," + bounds.getBottom() + "," + bounds.getRight() + "," + bounds.getTop() + "&WIDTH=512&HEIGHT=512";
   }
 
   public String getMapTimes(String type, String region) {
