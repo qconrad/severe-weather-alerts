d65bb798bacb6ac2b394d40d8a51ad13f150b48c
Quinn Conrad <quinnconrad15@gmail.com>
2021-02-21 19:28:10 -0600

    Wind gust subtitle

--- app/src/main/java/com/severeweatheralerts/Graphics/Generators/WindGustsGenerator.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/Generators/WindGustsGenerator.java
@@ -5,12 +5,11 @@ import android.content.Context;
 import com.severeweatheralerts.Alerts.Alert;
 import com.severeweatheralerts.Graphics.GridData.ForecastTime;
 import com.severeweatheralerts.Graphics.GridData.Maximum;
-import com.severeweatheralerts.Graphics.GridData.Parameter;
 import com.severeweatheralerts.Graphics.GridData.ParemeterTrim;
 import com.severeweatheralerts.Graphics.NextMapTimeFromDate;
+import com.severeweatheralerts.Graphics.Rounder;
 import com.severeweatheralerts.Location.Location;
-
-import java.util.Date;
+import com.severeweatheralerts.TextUtils.Plurality;
 
 public class WindGustsGenerator extends GraphicGenerator {
   public WindGustsGenerator(Context context, Alert alert, Location location) {
@@ -21,11 +20,29 @@ public class WindGustsGenerator extends GraphicGenerator {
 
   @Override
   protected void getURLs() {
-    Parameter trimmed = new ParemeterTrim(gridData).trimLeft(alert.getStartTime()).trimRight(alert.getEndTime()).getTrimmed();
-    ForecastTime forecastTime = new Maximum(trimmed).get();
-    String dateString = new NextMapTimeFromDate(mapTimes, forecastTime.getDate()).getMapTime().getString();
+    String dateString = new NextMapTimeFromDate(mapTimes, getMaxGust().getDate()).getMapTime().getString();
     urls.add(new URL().getWindGusts(bound, "conus", dateString));
     urls.add(new URL().getWindGustsPoints(bound, "conus", dateString));
     urls.add(new URL().getCountyMap(bound));
+  }
+
+  @Override
+  protected String getSubText() {
+    return getWindGustsMph() + " mph";
+  }
+
+  private int getWindGustsMph() {
+    return (int) (getMaxGust().getValue() / 1.609);
+  }
+
+  private ForecastTime maxGust = null;
+  private ForecastTime getMaxGust() {
+    if (maxGust == null)
+      maxGust = new Maximum(new ParemeterTrim(gridData)
+              .trimLeft(alert.getStartTime())
+              .trimRight(alert.getEndTime())
+              .getTrimmed())
+              .get();
+    return maxGust;
   }
 }
