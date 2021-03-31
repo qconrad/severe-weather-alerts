ae39491eb174fae29e8ccf8d1bdf9682ff714555
Quinn Conrad <quinnconrad15@gmail.com>
2021-03-30 20:15:51 -0500

    Alert channel preference activity

--- app/src/main/java/com/severeweatheralerts/Activities/SettingsActivity.java
+++ app/src/main/java/com/severeweatheralerts/Activities/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.severeweatheralerts.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.severeweatheralerts.R;
@@ -30,6 +32,47 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
       setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+      createClickListeners();
+    }
+
+    private void createClickListeners() {
+      createChannelListener();
+      createSeverityPreferencesListener();
+    }
+
+    private void createSeverityPreferencesListener() {
+      Preference severityPrefs = findPreference("severepref");
+      if (severityPrefs != null) {
+        severityPrefs.setOnPreferenceClickListener(preference -> {
+          showSeverityPreferences();
+          return true;
+        });
+      }
+    }
+
+    private void createChannelListener() {
+      Preference channels = findPreference("channels");
+      if (channels != null) {
+        channels.setOnPreferenceClickListener(preference -> {
+          showNotificationChannels();
+          return true;
+        });
+      }
+    }
+
+    private void showSeverityPreferences() {
+      Intent alertListIntent = new Intent(getActivity(), AlertChannelPicker.class);
+      startActivity(alertListIntent);
+    }
+
+    private void showNotificationChannels() {
+      Intent intent = new Intent();
+      intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+      intent.putExtra("app_package", getContext().getPackageName());
+      intent.putExtra("app_uid", getContext().getApplicationInfo().uid);
+      intent.putExtra("android.provider.extra.APP_PACKAGE", getContext().getPackageName());
+      startActivity(intent);
     }
   }
 }
