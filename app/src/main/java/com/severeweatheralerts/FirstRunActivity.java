4c165e6819dc9a013dacbd161aa7a9f3df6bd7bf
Quinn Conrad <quinnconrad15@gmail.com>
2021-04-17 01:34:23 -0500

    First run activity

---
+++ app/src/main/java/com/severeweatheralerts/FirstRunActivity.java
@@ -0,0 +1,14 @@
+package com.severeweatheralerts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstRunActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_first_run);
+  }
+}
