0414a7ef8327d58a12b0839d6daaefcafe0909bd
Quinn Conrad <quinnconrad15@gmail.com>
2021-04-24 00:54:00 -0500

    Preference UI changes

--- app/src/main/java/com/severeweatheralerts/Preferences/ChannelColors.java
+++ app/src/main/java/com/severeweatheralerts/Preferences/ChannelColors.java
@@ -4,10 +4,10 @@ import android.graphics.Color;
 
 public class ChannelColors {
   public static int getChannelColor(Channel channel) {
-    if (channel == Channel.EXTREME) return Color.parseColor("#FF00FF");
-    else if (channel == Channel.HIGH) return Color.parseColor("#FF0000");
-    else if (channel == Channel.MEDIUM) return Color.parseColor("#FFFF00");
-    else if (channel == Channel.LOW) return Color.parseColor("#00FF00");
-    else return Color.parseColor("#CCCCCC");
+    if (channel == Channel.EXTREME) return Color.parseColor("#9c36b5");
+    else if (channel == Channel.HIGH) return Color.parseColor("#c92a2a");
+    else if (channel == Channel.MEDIUM) return Color.parseColor("#ccaa2f");
+    else if (channel == Channel.LOW) return Color.parseColor("#2b8a3e");
+    else return Color.parseColor("#868e96");
   }
 }
