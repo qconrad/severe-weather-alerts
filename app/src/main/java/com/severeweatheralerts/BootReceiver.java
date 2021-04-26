be8eb16194e393cc828e630542a94292a61a9452
Quinn Conrad <quinnconrad15@gmail.com>
2021-04-26 17:55:01 -0500

    Check if locations exist on boot

--- app/src/main/java/com/severeweatheralerts/BootReceiver.java
+++ app/src/main/java/com/severeweatheralerts/BootReceiver.java
@@ -15,7 +15,7 @@ import static com.severeweatheralerts.Constants.STATIONARY_RADIUS;
 public class BootReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
-    if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) && !usingFixedLocation(context)) {
+    if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) && !usingFixedLocation(context) && LocationsDao.getInstance(context).hasLocations()) {
       GCSCoordinate coordinate = LocationsDao.getInstance(context).getCoordinate(0);
       new GeofenceManager(context).setStationaryGeofence(coordinate.getLat(), coordinate.getLong(), STATIONARY_RADIUS);
     }
