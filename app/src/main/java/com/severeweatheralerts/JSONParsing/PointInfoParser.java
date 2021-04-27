75b7079bfc7c4213d446e112d73f5a84fb7c8120
Quinn Conrad <quinnconrad15@gmail.com>
2021-04-27 18:05:40 -0500

    Radar station parsing from point info

--- app/src/main/java/com/severeweatheralerts/JSONParsing/PointInfoParser.java
+++ app/src/main/java/com/severeweatheralerts/JSONParsing/PointInfoParser.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 
 public class PointInfoParser {
   private String forecastGridLink;
+  private String radarStation;
 
   public PointInfoParser(String pointInfoJson) {
     try { parseInfo(pointInfoJson); }
@@ -14,9 +15,14 @@ public class PointInfoParser {
   private void parseInfo(String pointInfoJson) throws JSONException {
     JSONObject pointJson = new JSONObject(pointInfoJson);
     forecastGridLink = pointJson.getJSONObject("properties").getString("forecastGridData");
+    radarStation = pointJson.getJSONObject("properties").getString("radarStation");
   }
 
   public String getForecastGridLink() {
     return forecastGridLink;
+  }
+
+  public String getRadarStation() {
+    return radarStation;
   }
 }
