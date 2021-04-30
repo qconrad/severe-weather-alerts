8f10b6d51cabf3b679a04ada9c26ae19089b90b9
Quinn Conrad <quinnconrad15@gmail.com>
2021-04-29 22:21:13 -0500

    Hide next update time on alerts that don't provide them

--- app/src/main/java/com/severeweatheralerts/TextGeneraters/NextUpdate.java
+++ app/src/main/java/com/severeweatheralerts/TextGeneraters/NextUpdate.java
@@ -1,6 +1,16 @@
 package com.severeweatheralerts.TextGeneraters;
 
 import com.severeweatheralerts.Alerts.Alert;
+import com.severeweatheralerts.Alerts.NWS.DustStormWarning;
+import com.severeweatheralerts.Alerts.NWS.FlashFloodWarning;
+import com.severeweatheralerts.Alerts.NWS.FloodAdvisory;
+import com.severeweatheralerts.Alerts.NWS.FloodWarning;
+import com.severeweatheralerts.Alerts.NWS.SevereThunderstormWarning;
+import com.severeweatheralerts.Alerts.NWS.SevereThunderstormWatch;
+import com.severeweatheralerts.Alerts.NWS.SnowSquallWarning;
+import com.severeweatheralerts.Alerts.NWS.SpecialMarineWarning;
+import com.severeweatheralerts.Alerts.NWS.TornadoWarning;
+import com.severeweatheralerts.Alerts.NWS.TornadoWatch;
 import com.severeweatheralerts.TimeFormatters.AbsoluteTimeFormatter;
 
 import java.util.Date;
@@ -16,7 +26,7 @@ public class NextUpdate {
   }
 
   public boolean hasText() {
-    if (isCancel()) return false;
+    if (isCancel() || alertDoesNotProvideNextUpdate()) return false;
     return alert.getExpectedUpdateTime() != null;
   }
 
@@ -26,6 +36,20 @@ public class NextUpdate {
       return "Next update expected by " + new AbsoluteTimeFormatter(time, alert.getExpectedUpdateTime(), timeZone).getFormattedString();
     }
     return null;
+  }
+
+  private boolean alertDoesNotProvideNextUpdate() {
+    return alert.isLikelyLastUpdate() && (
+            alert instanceof SevereThunderstormWarning ||
+            alert instanceof SevereThunderstormWatch ||
+            alert instanceof DustStormWarning ||
+            alert instanceof SpecialMarineWarning ||
+            alert instanceof SnowSquallWarning ||
+            alert instanceof TornadoWatch ||
+            alert instanceof TornadoWarning ||
+            alert instanceof FloodWarning ||
+            alert instanceof FloodAdvisory ||
+            alert instanceof FlashFloodWarning);
   }
 
   private boolean isCancel() {
--- app/src/test/java/com/severeweatheralerts/NextUpdateTextGeneratorTests.java
+++ app/src/test/java/com/severeweatheralerts/NextUpdateTextGeneratorTests.java
@@ -2,6 +2,17 @@ package com.severeweatheralerts;
 
 import com.severeweatheralerts.Alerts.Alert;
 import com.severeweatheralerts.Alerts.DefaultAlert;
+import com.severeweatheralerts.Alerts.NWS.DustStormWarning;
+import com.severeweatheralerts.Alerts.NWS.FlashFloodWarning;
+import com.severeweatheralerts.Alerts.NWS.FloodAdvisory;
+import com.severeweatheralerts.Alerts.NWS.FloodWarning;
+import com.severeweatheralerts.Alerts.NWS.SevereThunderstormWarning;
+import com.severeweatheralerts.Alerts.NWS.SevereThunderstormWatch;
+import com.severeweatheralerts.Alerts.NWS.SnowSquallWarning;
+import com.severeweatheralerts.Alerts.NWS.SpecialMarineWarning;
+import com.severeweatheralerts.Alerts.NWS.TornadoWarning;
+import com.severeweatheralerts.Alerts.NWS.TornadoWatch;
+import com.severeweatheralerts.Graphics.Polygon.Polygon;
 import com.severeweatheralerts.TextGeneraters.NextUpdate;
 
 import org.junit.Test;
@@ -76,4 +87,102 @@ public class NextUpdateTextGeneratorTests {
     assertEquals("Next update expected by 3 PM", nextUpdateTextGenerator.getText(new Date(163000000L)));
   }
 
+  @Test
+  public void generateText_SevereThunderstormWarning_DoesNotHaveText() {
+    SevereThunderstormWarning alert = new SevereThunderstormWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setEndTime(new Date(164000000L));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_SevereThunderstormWatch_DoesNotHaveText() {
+    SevereThunderstormWatch alert = new SevereThunderstormWatch();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setEndTime(new Date(164000000L));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_TornadoWarningWithPolygon_DoesNotHaveText() {
+    TornadoWarning alert = new TornadoWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setEndTime(new Date(164000000L));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_TornadoWatch_DoesNotHaveText() {
+    TornadoWatch alert = new TornadoWatch();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setEndTime(new Date(164000000L));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_FlashFloodWarning_DoesNotHaveText() {
+    FlashFloodWarning alert = new FlashFloodWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setEndTime(new Date(164000000L));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_FlashFloodWarningNotLastUpdate_HasText() {
+    FlashFloodWarning alert = new FlashFloodWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(162000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertTrue(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_FloodWarning_DoesNotHaveText() {
+    FloodWarning alert = new FloodWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_FloodAdvisory_DoesNotHaveText() {
+    FloodAdvisory alert = new FloodAdvisory();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_DustStormWarning_DoesNotHaveText() {
+    DustStormWarning alert = new DustStormWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_SpecialMarineWarning_DoesNotHaveText() {
+    SpecialMarineWarning alert = new SpecialMarineWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
+
+  @Test
+  public void generateText_SnowSquallWarning_DoesNotHaveText() {
+    SnowSquallWarning alert = new SnowSquallWarning();
+    NextUpdate nextUpdateTextGenerator = new NextUpdate(alert, TimeZone.getTimeZone("CST"));
+    alert.setExpectedUpdateTime(new Date(164000000L));
+    alert.setEndTime(new Date(164000000L));
+    assertFalse(nextUpdateTextGenerator.hasText());
+  }
 }
