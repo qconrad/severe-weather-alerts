55486b3af79aa1b1bcfc198f561f2aa03e3b611c
Quinn Conrad <quinnconrad15@gmail.com>
2021-05-20 19:30:02 -0500

    Major refactoring of one hour precip

--- app/src/main/java/com/severeweatheralerts/Graphics/Generators/OneHourPrecipitation.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/Generators/OneHourPrecipitation.java
@@ -2,18 +2,17 @@ package com.severeweatheralerts.Graphics.Generators;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.Point;
 
 import com.android.volley.VolleyError;
 import com.severeweatheralerts.Adapters.GCSCoordinate;
 import com.severeweatheralerts.Alerts.Alert;
 import com.severeweatheralerts.Constants;
-import com.severeweatheralerts.Graphics.BitmapTools.LocationDrawer;
 import com.severeweatheralerts.Graphics.Bounds.AspectRatioEqualizer;
 import com.severeweatheralerts.Graphics.Bounds.BoundCalculator;
 import com.severeweatheralerts.Graphics.Bounds.Bounds;
 import com.severeweatheralerts.Graphics.DiagonalOffset;
+import com.severeweatheralerts.Graphics.GridData.ForecastTime;
 import com.severeweatheralerts.Graphics.Layer;
 import com.severeweatheralerts.Graphics.Polygon.MercatorCoordinate;
 import com.severeweatheralerts.Graphics.Polygon.MercatorCoordinateToPointAdapter;
@@ -30,13 +29,18 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class OneHourPrecipitation extends GraphicGenerator {
-  private final int angle = 181;
-  private final double metersPerSecond = 22 / 1.944;
-  private double latencyOffsetSeconds = 0;
+  private final int angle = 167;
+  private final double metersPerSecond = 53 / 1.944;
   private String radarStation;
+  private Date radarTime;
+  private final MercatorCoordinate location;
+  private final Bounds bounds;
+  private Bitmap map;
 
   public OneHourPrecipitation(Context context, Alert alert, GCSCoordinate location) {
     super(context, alert, location);
+    this.location = getMercatorCoordinate();
+    bounds = getBounds();
   }
 
   @Override
@@ -52,80 +56,83 @@ public class OneHourPrecipitation extends GraphicGenerator {
   }
 
   private void fetchFinished() {
-    if (radarStation != null) generateLayers();
+    if (radarStation != null) getRadarImageTime();
   }
 
-  private void generateLayers() {
+  private void getRadarImageTime() {
     StringFetchService fetch = new StringFetchService(context, new URL().getRadarCapabilities(radarStation, "bdhc"));
     fetch.setUserAgent(Constants.USER_AGENT);
     fetch.request(new RequestCallback() {
+
       @Override
       public void success(Object response) {
-        ArrayList<String> match = RegExMatcher.match("time\\\" default=\\\".*\\\"", response.toString());
-        if (match.size() > 0) {
-          String timeString = match.get(0).split("\\\"")[2];
-          Date date = DateTimeConverter.convertStringToDate(timeString, "yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC"));
-          latencyOffsetSeconds = Math.round((new Date().getTime() - date.getTime()) / 1000.0);
+        radarTime = parseTime(response);
+        if (radarTime == null) throwError("Error parsing image times");
 
-          ArrayList<Layer> layers = new ArrayList<>();
-          Bounds bounds = getBounds();
-          layers.add(new Layer(new URL().getDualPolPrecipitationType(bounds, radarStation)));
-          layers.add(new Layer(new LocationDrawer(bounds, getMercatorCoordinate(), Color.YELLOW).getBitmap()));
-          generateGraphicFromLayers(layers);
-        } else {
-          throwError("Error getting time info");
-        }
+        ArrayList<Layer> layers = new ArrayList<>();
+        layers.add(new Layer(new URL().getDualPolPrecipitationType(bounds, radarStation)));
+        generateGraphicFromLayers(layers);
       }
 
       @Override
       public void error(VolleyError error) {
-        throwError("Error getting time info");
+        throwError("Error getting image times");
       }
     });
   }
 
+  private Date parseTime(Object response) {
+    ArrayList<String> timeMatch = RegExMatcher.match("time\\\" default=\\\".*\\\"", response.toString());
+    if (timeMatch.size() < 1) return null;
+    return DateTimeConverter.convertStringToDate(timeMatch.get(0).split("\\\"")[2], "yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC"));
+  }
+
   private Bounds getBounds() {
     Polygon polygon = new Polygon();
-    polygon.addCoordinate(getMercatorCoordinate());
-    DiagonalOffset offset = new DiagonalOffset(-metersPerSecond * (3600 + latencyOffsetSeconds), angle);
-    polygon.addCoordinate(new MercatorCoordinate(getMercatorCoordinate().getX()+offset.getX(), getMercatorCoordinate().getY()+offset.getY()));
+    polygon.addCoordinate(getCoordinateAt(location, 0));
+    polygon.addCoordinate(getCoordinateAt(location, 3600));
     return new AspectRatioEqualizer(new BoundCalculator(polygon).getBounds()).equalize();
+  }
+
+  private MercatorCoordinate getCoordinateAt(MercatorCoordinate start, int secondOffset) {
+    return offsetCoordinate(start, new DiagonalOffset(-metersPerSecond * secondOffset, angle));
+  }
+
+  private MercatorCoordinate offsetCoordinate(MercatorCoordinate start, DiagonalOffset offset) {
+    return new MercatorCoordinate(start.getX() + offset.getX(), start.getY() + offset.getY());
   }
 
   @Override
   protected void layers(ArrayList<Bitmap> bitmaps) {
-    Bitmap bitmap = bitmaps.get(0);
-    StringBuilder colors = new StringBuilder();
-    for (int i = 0; i <= 60; i++) {
-      colors.append("At minute "+ i + ": ");
-      int color = getNextMinute(bitmap, getBounds(), getMercatorCoordinate(), i);
-      Color color1 = null;
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-        color1 = Color.valueOf(color);
-      }
-      if (color == -16729344) colors.append("Heavy Rain").append("\n");
-      else if (color == -16712816) colors.append("Light/Moderate Rain").append("\n");
-      else if (color == -1638145) colors.append("Unknown").append("\n");
-      else if (color == -3092384) colors.append("Big Drops").append("\n");
-      else if (color == -65536) colors.append("Hail/Rain").append("\n");
-      else if (color == -6513508) colors.append("Biological").append("\n");
-      else if (color == -9013642) colors.append("Ground Clutter").append("\n");
-      else if (color == 0) colors.append("Nothing").append("\n");
-      else colors.append(color).append(" ").append(color1.toString()).append("\n");
-      if (i % 15 == 0) {
-        DiagonalOffset diagonalOffset = new DiagonalOffset(-metersPerSecond * ((60 * i) + latencyOffsetSeconds), angle);
-        MercatorCoordinate mercatorCoordinate = new MercatorCoordinate(getMercatorCoordinate().getX()+diagonalOffset.getX(), getMercatorCoordinate().getY()+diagonalOffset.getY());
-        bitmaps.add(new LocationDrawer(getBounds(), mercatorCoordinate, Color.RED).getBitmap());
-      }
+    map = bitmaps.get(0);
+    ArrayList<ForecastTime> forecast = new ArrayList<>();
+    String subtext = "";
+    for (int i = 0; i <= 3600; i += 10) {
+      PRECIPITATION_TYPE precipitationType = getPrecipitationType(getCoordinateAt(location, i));
+      forecast.add(new ForecastTime(getDateAt(i), precipitationType.ordinal()));
+      subtext += PRECIPITATION_TYPE.values()[(int) forecast.get(i/10).getValue()] + "\n";
     }
-    setSubtext(colors.toString());
+    setSubtext(subtext);
     super.layers(bitmaps);
   }
 
-  private int getNextMinute(Bitmap map, Bounds bounds, MercatorCoordinate start, int minute) {
-    DiagonalOffset diagonalOffset = new DiagonalOffset(-metersPerSecond * ((60 * minute) + latencyOffsetSeconds), angle);
-    MercatorCoordinate mercatorCoordinate = new MercatorCoordinate(start.getX()+diagonalOffset.getX(), start.getY()+diagonalOffset.getY());
-    Point point = new MercatorCoordinateToPointAdapter(bounds, 511, 511).getPoint(mercatorCoordinate);
+  private Date getDateAt(int secondsOffset) {
+    return new Date(radarTime.getTime() + (secondsOffset * 1000));
+  }
+
+  public enum PRECIPITATION_TYPE { NONE, BIG_DROPS, LIGHT_MOD_RAIN, HEAVY_RAIN, HAIL_RAIN }
+
+  private PRECIPITATION_TYPE getPrecipitationType(MercatorCoordinate coordinate) {
+    int color = getColorAt(coordinate);
+    if (color == -16729344) return PRECIPITATION_TYPE.HEAVY_RAIN;
+    if (color == -16712816) return PRECIPITATION_TYPE.LIGHT_MOD_RAIN;
+    if (color == -3092384) return PRECIPITATION_TYPE.BIG_DROPS;
+    if (color == -65536) return PRECIPITATION_TYPE.HAIL_RAIN;
+    return PRECIPITATION_TYPE.NONE;
+  }
+
+  private int getColorAt(MercatorCoordinate coordinate) {
+    Point point = new MercatorCoordinateToPointAdapter(bounds, 511, 511).getPoint(coordinate);
     return map.getPixel(point.x, 511 - point.y);
   }
 }
