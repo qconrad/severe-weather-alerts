0f64581f28aa0e4d1ab2ae7ed92b03756edd889f
Quinn Conrad <quinnconrad15@gmail.com>
2021-07-18 17:19:15 -0500

    Manually fetch large payloads

--- app/src/main/java/com/severeweatheralerts/Notifications/MessageService.java
+++ app/src/main/java/com/severeweatheralerts/Notifications/MessageService.java
@@ -4,23 +4,44 @@ import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.severeweatheralerts.AlertListTools.AlertFinder;
 import com.severeweatheralerts.Alerts.Alert;
 import com.severeweatheralerts.Location.LocationsDao;
+import com.severeweatheralerts.Networking.LocationPopulaters.FromLocationPointPopulater;
+import com.severeweatheralerts.Networking.LocationPopulaters.PopulateCallback;
 import com.severeweatheralerts.Preferences.Channel;
 import com.severeweatheralerts.UserSync.UserSyncWorkScheduler;
+
+import java.util.ArrayList;
 
 import static com.severeweatheralerts.Preferences.ChannelIdString.getChannelString;
 
 public class MessageService extends FirebaseMessagingService {
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
+    if (remoteMessage.getData().size() < 1) return;
     LocationsDao.messageReceived();
-    if (remoteMessage.getData().size() > 0) {
-      MessageAdapter messageAdapter = new MessageAdapter(remoteMessage.getData());
-      Alert alert = messageAdapter.getAlert();
-      Channel channel = LocationsDao.getInstance(this).getChannelPreferences(0).getChannel(alert.getName(), alert.getType());
-      if (channel != Channel.NONE) new NotificationSender(this, alert, getChannelString(channel)).send();
-    }
+    if (remoteMessage.getData().containsKey("fetchManually")) fetchAlert(remoteMessage.getData().get("id"));
+    else sendAlert(new MessageAdapter(remoteMessage.getData()).getAlert());
+  }
+
+  private void fetchAlert(String id) {
+    LocationsDao locationsDao = LocationsDao.getInstance(this);
+    new FromLocationPointPopulater(locationsDao.getCoordinate(0), this).populate(new PopulateCallback() {
+      @Override
+      public void complete(ArrayList<Alert> alerts) {
+        locationsDao.setAlerts(0, alerts);
+        Alert alert = new AlertFinder(alerts).findAlertByID(id);
+        if (alert != null) sendAlert(alert);
+      }
+      @Override
+      public void error(String message) { }
+    });
+  }
+
+  private void sendAlert(Alert alert) {
+    Channel channel = LocationsDao.getInstance(this).getChannelPreferences(0).getChannel(alert.getName(), alert.getType());
+    if (channel != Channel.NONE) new NotificationSender(this, alert, getChannelString(channel)).send();
   }
 
   @Override
