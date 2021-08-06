8602e854adb02a679e56ef8133acf7b9e77987d5
Quinn Conrad <quinnconrad15@gmail.com>
2021-08-05 21:52:58 -0500

    Manage locations population

--- app/src/main/java/com/severeweatheralerts/ManageLocationsActivity.java
+++ app/src/main/java/com/severeweatheralerts/ManageLocationsActivity.java
@@ -1,8 +1,17 @@
 package com.severeweatheralerts;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.severeweatheralerts.Location.Location;
+import com.severeweatheralerts.Location.LocationsDao;
+
+import java.util.ArrayList;
 
 public class ManageLocationsActivity extends AppCompatActivity {
 
@@ -10,5 +19,29 @@ public class ManageLocationsActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_manage_locations);
+    populateLocations();
+  }
+
+  private void populateLocations() {
+    ArrayList<Location> locations = LocationsDao.getInstance(this).getLocations();
+    if (noExtras(locations)) return;
+    findViewById(R.id.no_locations_text).setVisibility(View.GONE);
+    addLocationsToLayout(locations);
+  }
+
+  private boolean noExtras(ArrayList<Location> locations) {
+    return locations.size() <= 1;
+  }
+
+  private void addLocationsToLayout(ArrayList<Location> locations) {
+    LinearLayout locationStack = findViewById(R.id.location_stack);
+    for (int i = 1; i < locations.size(); i++)
+      addLocationToStack(locations, locationStack, i);
+  }
+
+  private void addLocationToStack(ArrayList<Location> locations, LinearLayout locationStack, int i) {
+    Button location = new Button(this);
+    location.setText(locations.get(i).getName());
+    locationStack.addView(location);
   }
 }
