c9575b376cade4570560f60c4684a17f37b2393a
Quinn Conrad <quinnconrad15@gmail.com>
2021-08-27 22:57:24 -0500

    Tropical graphic abstraction

--- app/src/main/java/com/severeweatheralerts/Graphics/Generators/HurricaneWindThreatGenerator.java
+++ app/src/main/java/com/severeweatheralerts/Graphics/Generators/HurricaneWindThreatGenerator.java
@@ -1,45 +1,20 @@
 package com.severeweatheralerts.Graphics.Generators;
 
 import android.content.Context;
-import android.graphics.Color;
 
 import com.severeweatheralerts.Adapters.GCSCoordinate;
 import com.severeweatheralerts.Alerts.Alert;
-import com.severeweatheralerts.Graphics.Bounds.BoundRounder;
 import com.severeweatheralerts.Graphics.Bounds.Bounds;
-import com.severeweatheralerts.Graphics.GridData.MapTime;
 import com.severeweatheralerts.Graphics.Layer;
-import com.severeweatheralerts.Graphics.Polygon.MercatorCoordinate;
 import com.severeweatheralerts.Graphics.URL;
 
-import java.util.ArrayList;
-
-public class HurricaneWindThreatGenerator extends GraphicGenerator {
-  private static final int ZOOM_SIZE = 400;
+public class HurricaneWindThreatGenerator extends TropicalGraphicGenerator {
   public HurricaneWindThreatGenerator(Context context, Alert alert, GCSCoordinate location) {
-    super(context, alert, location);
+    super(context, alert, location, "tcwind");
   }
 
   @Override
-  public void generate(GraphicCompleteListener graphicCompleteListener) {
-    super.generate(graphicCompleteListener);
-    getMapTimes("tcwind");
-  }
-
-  protected Bounds getBounds(int size, MercatorCoordinate loc) {
-    double margin = size * 1000;
-    return new BoundRounder(new Bounds(loc.getY()+margin,loc.getX()+margin,loc.getY()-margin,loc.getX()-margin)).getBounds();
-  }
-
-  @Override
-  protected void mapTimes(ArrayList<MapTime> mapTimes) {
-    MercatorCoordinate loc = getMercatorCoordinate();
-    Bounds bounds = getBounds(ZOOM_SIZE, loc);
-    String dateString = mapTimes.get(0).getString();
-    ArrayList<Layer> layers = new ArrayList<>();
-    layers.add(new Layer(new URL().getHurricaneWindThreat(bounds, getRegion(), dateString)));
-    layers.add(new Layer(new URL().getCountyMap(bounds)));
-    layers.add(new Layer(getLocationPointOverlay(bounds, Color.CYAN)));
-    generateGraphicFromLayers(layers);
+  protected Layer getLayer(Bounds bounds, String dateString) {
+    return new Layer(new URL().getHurricaneWindThreat(bounds, getRegion(), dateString));
   }
 }
