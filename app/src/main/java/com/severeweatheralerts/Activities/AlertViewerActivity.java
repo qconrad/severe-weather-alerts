f11cd8664dedf49f47caf5804ff40d0887c9caa1
Quinn Conrad <quinnconrad15@gmail.com>
2022-02-05 20:16:16 -0600

    Proper content insets for alert viewer

--- app/src/main/java/com/severeweatheralerts/Activities/AlertViewerActivity.java
+++ app/src/main/java/com/severeweatheralerts/Activities/AlertViewerActivity.java
@@ -18,6 +18,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -55,7 +59,6 @@ public class AlertViewerActivity extends AppCompatActivity {
     setContentView(R.layout.activity_alert_viewer);
     locationsDao = LocationsDao.getInstance(this);
 
-    makeStatusBarTransparent();
     getAlertFromExtras(getIntent().getExtras());
     if (!validAlert()) return;
     populateUIWithAlertData();
@@ -63,6 +66,18 @@ public class AlertViewerActivity extends AppCompatActivity {
     isActive = al.activeAt(new Date());
     refresher.add(new IntervalRun(5000, this::refresh));
     refresher.add(new IntervalRun(60000, this::populateReferences));
+
+    makeStatusBarTransparent();
+    setContentInsets();
+  }
+
+  private void setContentInsets() {
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.alert_viewer), (v, windowInsets) -> {
+      Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+      findViewById(R.id.alert_viewer).setPadding(insets.left, insets.top, insets.right, insets.bottom);
+      return WindowInsetsCompat.CONSUMED;
+    });
   }
 
   private void refresh() {
@@ -233,11 +248,11 @@ public class AlertViewerActivity extends AppCompatActivity {
 
   private void setBackgroundColor() {
     int alertColor = al.getColorAt(new Date());
-    View titleCard = findViewById(R.id.alert_viewer);
+    View alertViewerLayout = findViewById(R.id.alert_viewer);
     int topGradientStep = ColorBrightnessChanger.changeBrightness(alertColor, 0.5f);
     int bottomGradientStep = ColorBrightnessChanger.changeBrightness(alertColor, 0.1f);
     GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[] {topGradientStep, bottomGradientStep});
-    titleCard.setBackground(gd);
+    alertViewerLayout.setBackground(gd);
   }
 
   private void setTitleCardColor() {
