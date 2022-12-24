7d8d47667df4c14c3ff3b4adc8a984cb82078265
Quinn Conrad <quinnconrad15@gmail.com>
2022-12-24 00:30:46 -0600

    Implement preliminary version of improved geocoding

--- app/src/main/java/com/severeweatheralerts/Activities/LocationPickerActivity.java
+++ app/src/main/java/com/severeweatheralerts/Activities/LocationPickerActivity.java
@@ -2,22 +2,23 @@ package com.severeweatheralerts.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.VolleyError;
-import com.severeweatheralerts.JSONParsing.GeocodeParser;
-import com.severeweatheralerts.Networking.FetchServices.RequestCallback;
-import com.severeweatheralerts.Networking.FetchServices.StringFetchService;
 import com.severeweatheralerts.R;
 
+import java.io.IOException;
+import java.util.List;
+
 public class LocationPickerActivity extends AppCompatActivity {
+  private double lat;
+  private double lon;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,75 +27,40 @@ public class LocationPickerActivity extends AppCompatActivity {
   }
 
   public void searchClick(View view) {
-    TextView textView = findViewById(R.id.location_search_ev);
-    String text = textView.getText().toString();
-    if (text.equals("")) return;
-    view.setEnabled(false);
-    ProgressBar progressBar = findViewById(R.id.location_search_progress);
-    progressBar.setVisibility(View.VISIBLE);
-    String search = text.replace(" ", "+");
-    Button searchBtn = findViewById(R.id.location_search_button);
-    new StringFetchService(this, "https://geocode.xyz/" + search + "?json=1&region=US").request(new RequestCallback() {
-      @Override
-      public void success(Object response) {
-        GeocodeParser geocodeParser = new GeocodeParser(response.toString());
-        searchBtn.setEnabled(false);
-        Button clear = findViewById(R.id.clear_button);
-        clear.setVisibility(View.VISIBLE);
-        progressBar.setVisibility(View.INVISIBLE);
-        TextView textView1 = findViewById(R.id.location_text);
-        if (geocodeParser.hasError()) {
-          textView1.setText(geocodeParser.getError());
-        }
-        else {
-          String location = "";
-          if (geocodeParser.getAddress() != null && !geocodeParser.getAddress().equals(geocodeParser.getAddress().toUpperCase())) location += geocodeParser.getAddress() + "\n";
-          if (geocodeParser.getCity() != null) location += geocodeParser.getCity() + "\n";
-          if (geocodeParser.getPostal() != null) location += geocodeParser.getPostal();
-          textView1.setText(location.trim());
-          lat = geocodeParser.getLatitude();
-          lon = geocodeParser.getLongitude();
-          Button useThisLocation = findViewById(R.id.use_location);
-          useThisLocation.setEnabled(true);
-        }
-      }
+    TextView noResultsText = findViewById(R.id.no_results_text);
+    noResultsText.setVisibility(View.INVISIBLE);
 
-      @Override
-      public void error(VolleyError error) {
-        Toast.makeText(LocationPickerActivity.this, "An error occurred while looking up location", Toast.LENGTH_SHORT).show();
-        searchBtn.setEnabled(true);
-        progressBar.setVisibility(View.INVISIBLE);
-      }
-    });
+    TextView textView = findViewById(R.id.location_search_ev);
+    String searchText = textView.getText().toString();
+    if (searchText.equals("")) return;
+
+    Geocoder geocoder = new Geocoder(this);
+    List<Address> addresses = null;
+    try {
+      addresses = geocoder.getFromLocationName(searchText, 5);
+    } catch (IOException e) {
+      e.printStackTrace();
+      Toast.makeText(this, "Error looking up location", Toast.LENGTH_SHORT).show();
+      return;
+    }
+    if (addresses.size() == 0) {
+      noResultsText.setVisibility(View.VISIBLE);
+      return;
+    }
+    // Take the first location for now
+    lat = addresses.get(0).getLatitude();
+    lon = addresses.get(0).getLongitude();
   }
 
   public void cancel(View view) {
     finish();
   }
 
-  public void clearClick(View view) {
-    Button useThisLocation = findViewById(R.id.use_location);
-    useThisLocation.setEnabled(true);
-    Button search = findViewById(R.id.location_search_button);
-    search.setEnabled(true);
-    TextView textView1 = findViewById(R.id.location_text);
-    textView1.setText("");
-    Button clear = findViewById(R.id.clear_button);
-    clear.setVisibility(View.INVISIBLE);
-  }
-
-  double lat;
-  double lon;
-
   public void useLocation(View view) {
     TextView nameEt = findViewById(R.id.location_name_et);
     String name = nameEt.getText().toString();
     if (name.equals("")) {
       Toast.makeText(this, "Enter a name for this location", Toast.LENGTH_SHORT).show();
-      return;
-    }
-    else if (name.length() > 20) {
-      Toast.makeText(this, "This name is too long. Try picking a shorter one.", Toast.LENGTH_SHORT).show();
       return;
     }
     Intent intent = new Intent().putExtra("name", name).putExtra("lat", lat).putExtra("lon", lon);
